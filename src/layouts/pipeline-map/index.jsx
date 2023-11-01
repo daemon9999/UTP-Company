994e6ac4c30203e0cf7925830daff0413a4a4a04
Murad Masimli <murad.mesimli.99@gmail.com>
2023-11-01 10:06:55 +0400

    finished

--- src/layouts/pipeline-map/index.jsx
+++ src/layouts/pipeline-map/index.jsx
@@ -9,8 +9,8 @@ export default function PipelineMap() {
         <p className="text-sm sm:text-base md::text-lg  font-semibold">The Total Path Length of the Route is equal to 1445.96 km. Additionally, for visualization, this is the demo version of the path which was prepared in initial days.</p>
        
         <div className="w-auto h-[200px] sm:h-[300px] relative md:h-[500px] mx-auto ">
-            <a href="https://sjskjsjaqeuexbandiqoxd.on.drv.tw/map.html" target="_blank" className="absolute top-4 right-4 sm:top-7 sm:right-7 md:top-10 md:right-10 bg-blue-400 inline-flex items-center justify-center text-white  font-semibold text-sm sm:text-base md:text-lg px-2 py-1 rounded-full hover:bg-blue-300 transition-all ">
-                Map View
+            <a href="https://sjskjsjaqeuexbandiqoxd.on.drv.tw/map.html" target="_blank" className="absolute bottom-4 right-4 sm:bottom-7 sm:right-7 md:bottom-10 md:right-10 bg-blue-400 inline-flex items-center justify-center text-white  font-semibold text-base md:text-lg px-2 py-1 md:px-4 md:py-2 rounded-full hover:bg-blue-300 transition-all ">
+                Finalized One
             </a>
           <img
             src="/images/pipeline-map.png"
